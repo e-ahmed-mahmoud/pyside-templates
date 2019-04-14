440d1776eca7cbd85e2838be6ad0a28c550b7b61
rwprkr <49272998+rwprkr@users.noreply.github.com>
2019-04-14 15:37:42 -0300

    Update main.py

--- main-window/main.py
+++ main-window/main.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
-# Libraries needed: PySide2
+#
+#   main.py
+#   A main window template including customized menu bar.
+#   Using Python 3.6 and PySide2 v.5.12
+#
+#   Copyright (C) 2019 Robert Parker
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 
 from PySide2.QtWidgets import QMainWindow, QDesktopWidget, QMenuBar, QMenu, QAction, QStatusBar
